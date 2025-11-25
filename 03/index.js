b8fa7ce137f4ca634738e05f01195ad10ea84b91
Gaurav Pratap Singh <gs492979@gmail.com>
2025-11-26 02:54:16 +0530

    first object created.

---
+++ 03/index.js
@@ -0,0 +1,28 @@
+import * as THREE from "three";
+const canvas = document.querySelector("canvas.webgl");
+// scene
+const scene = new THREE.Scene();
+// object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// sizes
+const sizes = {
+    width: 800,
+    height: 600,
+}
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+
+scene.add(camera);
+
+// render 
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera)
